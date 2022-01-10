3417b0f27b3fe5417563af68bc705f4a40268370
user <user@DESKTOP-8R6IVLS>
2022-01-10 20:03:45 +0530

    First Commit

---
+++ C2TCB7_CoreJava_P2/src/General/Hello.java
@@ -0,0 +1,16 @@
+package General;
+
+public class Hello {
+
+	public static void main(String[] args) {
+byte x=10, y=20;
+		
+		String name="Swamynathan";
+		//byte z=(byte) (x+y);
+		System.out.println("Sum is " +(x+y)); 
+	
+		System.out.println("Hello " +name);
+
+	}
+
+}
---
+++ C2TCB7_CoreJava_P2/src/General/StringValueDemo.java
@@ -0,0 +1,13 @@
+package General;
+
+
+public class StringValueDemo {
+	public static void main(String[] args) {
+			String batchname= "C2TC-B7";
+			String branch=new String ("Bangalore");
+			System.out.println(batchname);
+			System.out.println(branch);
+
+	}
+
+}
