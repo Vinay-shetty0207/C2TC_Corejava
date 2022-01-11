949c19a8899c0a43c43b5d4878835be11ffcb31d
user <user@DESKTOP-8R6IVLS>
2022-01-11 12:02:43 +0530

    First Commit

---
+++ C2TCB7_CoreJava_P2/src/Exception_Handling_day11/Exception_handlingEx.java
@@ -0,0 +1,30 @@
+
+
+package Exception_Handling_day11;
+
+public class Exception_handlingEx {
+
+	public static void main(String[] args) {
+		int[] arr=new int[4];
+		try {
+			
+		
+			arr[6]=34;
+		System.out.println("in try block");
+		}
+		catch(ArrayIndexOutOfBoundsException ae)
+		{
+			
+			ae.getMessage();
+			
+		}
+		finally
+		{
+			System.out.println("finally block");
+		}
+		 
+
+	}
+
+}
+
---
+++ C2TCB7_CoreJava_P2/src/Exception_Handling_day11/TryCatch2.java
@@ -0,0 +1,11 @@
+package Exception_Handling_day11;
+
+public class TryCatch2 {
+
+	public static void main(String[] args) {
+		int data=5/0;
+		System.out.println("no try catch ");
+
+	}
+
+}
